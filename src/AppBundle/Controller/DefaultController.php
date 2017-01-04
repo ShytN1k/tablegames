942cebee574b07943938895db1406614a428f010
Denis Beliakov <denis.beliakov14@gmail.com>
2017-01-04 18:20:37 +0200

    tmp

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,9 +15,27 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        $number = mt_rand(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+
+
+    /**
+     * @Route(
+     *     "/{luckyNumber}",
+     *     name="cheat_lucky_number",
+     *     defaults={"luckyNumber": "36"},
+     *     requirements={"luckyNumber":"[0-9]{1,2}"}
+     * )
+     * @Method("GET")
+     */
+    public function cheatLuckyNumberAction(Request $request, $luckyNumber)
+    {
+        return new Response(
+            '<html><body>Lucky number: '.$luckyNumber.'</body></html>'
+        );
     }
 }
